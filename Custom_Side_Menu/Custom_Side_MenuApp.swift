3ea03db633687b38e3527438c4b8b41479b73eeb
avi7501 <avinashmuralidharan2@gmail.com>
2023-09-15 15:43:19 +0530

    Initial Commit

---
+++ Custom_Side_Menu/Custom_Side_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Side_MenuApp.swift
+//  Custom_Side_Menu
+//
+//  Created by Avinash Muralidharan on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Side_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
